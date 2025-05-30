6fdee7abd2396c268841d89c8040db680774b6bb
levy.caua <levy.caua@pwi.com.br>
2025-05-30 19:29:56 -0300

    Atualizando configs

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,7 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/PI-SENAC-5SEM-PT2/',
+  base: '/',
+  build: {
+    outDir: '../docs'
+  },
   plugins: [
     react(),
     tailwindcss()
