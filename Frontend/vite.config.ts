369cff39e0a0c3aa2afd53bf01d5ebe899485e8a
levy.caua <levy.caua@pwi.com.br>
2025-05-30 18:57:43 -0300

    Atualizando deploy no github pages

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PI-SENAC-5SEM-PT2/',
   plugins: [
     react(),
     tailwindcss()
